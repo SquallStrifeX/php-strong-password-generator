1418aa08ab5ef901e1158cb2f5d0a52b1c1ad027
SquallStrifeX <squallstrifex@gmail.com>
2024-01-26 17:35:58 +0100

    end milestone 2

---
+++ partials/functions.php
@@ -0,0 +1,19 @@
+<?php 
+if (isset($_GET['pass'])){
+    $lunghezza_password = $_GET['pass'];
+    $passwordGenerata = generaPasswordCasuale($lunghezza_password);
+}
+
+function generaPasswordCasuale($lunghezza_password) {
+    $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}|';
+    $lunghezzaCaratteri = strlen($caratteri);
+    $password = '';
+
+    for ($i = 0; $i < $lunghezza_password; $i++) {
+        $indiceCasuale = rand(0, $lunghezzaCaratteri - 1);
+        $password .= $caratteri[$indiceCasuale];
+    }
+
+    return $password;
+}
+?>
